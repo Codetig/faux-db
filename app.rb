665f263bbf489e9bf13651509a5051a43d83ea57
Ifeoluwa Omoniyi <codetig@outlook.com>
2015-02-11 21:57:42 -0800

    finished basic homework

--- app.rb
+++ app.rb
@@ -1,6 +1,5 @@
 require 'pry'
 require 'sinatra'
-require 'sinatra/reloader'
 require "better_errors"
 
 configure :development do
@@ -25,11 +24,39 @@ get '/' do
 end
 
 # GET /users/new - display a form for making a new user
+get '/users/new' do
+  erb :new_user
+end
 #
 # POST /users - create a user based on params from form
+post '/users' do
+  return "Please add values before clicking the 'Add User' button" if params[:first]==nil || params[:last]==nil
+  uhash = {}
+  uhash[:id] = id
+  uhash[:first] = params[:first]
+  uhash[:last] = params[:last]
+  id += 1
+  users<< uhash
+  redirect to '/'
+end
 #
 # GET /users/:id - show a user's info by their id, this should display the info in a form
+get '/users/:id' do
+  @user = users[params[:id].to_i - 1]
+  erb :user_detail
+end
 #
 # PUT /users/:id - update a user's info based on the form from GET /users/:id
+put '/users/:id' do
+  namer = users[params[:id].to_i - 1]
+  namer[:first] = params[:first]
+  namer[:last] = params[:last]
+  redirect to '/'
+end
 #
 # DELETE /users/:id - delete a user by their id
+delete '/users/:id' do
+  users.delete_at(params[:id].to_i - 1)
+  redirect to '/'
+end
+
